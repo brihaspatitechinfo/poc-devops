84b9af22a420fe6afab3baaab1e0bb3869956553
Yash <42830359+brihaspatitechinfo@users.noreply.github.com>
2025-09-10 17:59:44 +0530

    Update app.module.ts

--- services/user-service/src/app.module.ts
+++ services/user-service/src/app.module.ts
@@ -17,7 +17,7 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
       envFilePath: ['.env.local', '.env'],
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://mongodb:27017/weace_users'),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://myappuser:admin@123@54.221.52.72:27017/myappdb'),
     UsersModule,
     HealthModule,
     CryptoModule,
